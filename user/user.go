e63870b8c3eb185c11a84305e02cd8231e406f0d
simba <simba@iageengineering.net>
2022-07-09 16:06:22 +0300

    404 -> errtag.NotFound

--- user/user.go
+++ user/user.go
@@ -7,7 +7,7 @@ import (
 	"goerrtag/errtag"
 )
 
-var ErrNotFound = errtag.New("404", errors.New("user not found"))
+var ErrNotFound = errtag.New(errtag.NotFound, errors.New("user not found"))
 
 func Update() error {
 	return fmt.Errorf("failed to update a user: %w", ErrNotFound)
